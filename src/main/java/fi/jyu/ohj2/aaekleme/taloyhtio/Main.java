09f0daf66eccdcafc93ee0401b10c611f2898274
taurgus <100350635+taurgus@users.noreply.github.com>
2026-03-31 10:59:22 +0300

    initial

---
+++ src/main/java/fi/jyu/ohj2/aaekleme/taloyhtio/Main.java
@@ -0,0 +1,9 @@
+package fi.jyu.ohj2.aaekleme.taloyhtio;
+
+import javafx.application.Application;
+
+public class Main {
+     static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
